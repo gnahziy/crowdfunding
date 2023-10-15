275b1c7fb4940a2f2e9a351a7307b994d0caf3c5
gnahziy <gnahziy@yahoo.com>
2023-10-15 12:24:48 -0500

    transfering to repo

---
+++ README.md
@@ -0,0 +1,26 @@
+# Crowdfunding_ETL
+ETL (Extraction, Transform, Load) Project to clean data sourced from crowdfunding projects. 
+
+### Project Introduction
+This is a project to build an ETL pipeline using Python, Pandas, and Python dictionary methods to extract and transform the data. After transforming the data, four CSV files are created and these files are used to create an ERD and a table schema. Finally, these CSV files are used to create a Postgres database.
+
+### Team Members - with GitHub Handles
+* Dan Dreger - DanDreger
+* Tony Jordan - anthjordan
+* Yi Zhang - gnahziy
+* Alexis Rangel - alexxisrangel
+* Andrew Brannon - Andbra2
+
+### File Structure
+* The "extraction" and "transformation" work is contained in the ETL_Mini_Project_dDreger_yZhang_aRangel_aLopez.ipynb file
+* Starting data is in the "Resources" Directory in the Crouwdfunding.xls and contacts.xls files
+* 4 CSV files are created in the ETL_Mini_Project_dDreger_yZhang_aRangel_aLopez.ipynb file, which are used to create the ERD
+* These 4 CSV files are also in the "Resources" Directory
+
+### Tech Stack
+This ETL project uses the following
+* Python
+* Pandas
+* SQL
+* Jupyter Notebook
+
---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,73 @@
+﻿
+--Table creation for crowdfunding 
+CREATE TABLE "crowdfunding" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" INT   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_crowdfunding" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+--Table creation for contacts 
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+--Table creation for category_table
+CREATE TABLE "category_table" (
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_category_table" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+--Table creation for subcategory_table
+CREATE TABLE "subcategory_table" (
+    "subcategory_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_subcategory_table" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+--Altering tables in order to include foreign keys and relationships
+ALTER TABLE "crowdfunding" ADD CONSTRAINT "fk_crowdfunding_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "crowdfunding" ADD CONSTRAINT "fk_crowdfunding_category_id" FOREIGN KEY("category_id")
+REFERENCES "category_table" ("category_id");
+
+ALTER TABLE "crowdfunding" ADD CONSTRAINT "fk_crowdfunding_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory_table" ("subcategory_id");
+
+--Viewing each table in order to see that all data was added correctly 
+SELECT * 
+FROM crowdfunding;
+
+SELECT * 
+FROM contacts;
+
+SELECT * 
+FROM category_table;
+
+SELECT * 
+FROM subcategory_table;
